981a9a03b80c7752463167bc2d7e426ef6215057
Per Storgaard Laursen <perl@easj.dk>
2017-10-26 20:42:25 +0200

    Changed Image to TaggedImage in AppConfig

--- MVVMStarterDemoB/Configuration/App/AppConfig.cs
+++ MVVMStarterDemoB/Configuration/App/AppConfig.cs
@@ -43,55 +43,55 @@ namespace MVVMStarterDemoB.Configuration.App
             List<IImage> imageList = new List<IImage>();
 
             #region Car image objects
-            Image c1 = new Image("Red Sedan", prefix + "CarRedSedan.jpg");
+            TaggedImage c1 = new TaggedImage("Red Sedan", prefix + "CarRedSedan.jpg");
             c1.AddTag("Car");
             c1.AddTag("Red");
             c1.AddTag("Sedan");
             c1.AddTag("Individual");
 
-            Image c2 = new Image("Blue Combi", prefix + "CarBlueCombi.jpg");
+            TaggedImage c2 = new TaggedImage("Blue Combi", prefix + "CarBlueCombi.jpg");
             c2.AddTag("Car");
             c2.AddTag("Blue");
             c2.AddTag("Combi");
             c2.AddTag("Family");
 
-            Image c3 = new Image("White Mini", prefix + "CarWhiteMini.jpg");
+            TaggedImage c3 = new TaggedImage("White Mini", prefix + "CarWhiteMini.jpg");
             c3.AddTag("Car");
             c3.AddTag("White");
             c3.AddTag("Mini");
             c3.AddTag("Family");
 
-            Image c4 = new Image("Green Combi", prefix + "CarGreenCombi.jpg");
+            TaggedImage c4 = new TaggedImage("Green Combi", prefix + "CarGreenCombi.jpg");
             c4.AddTag("Car");
             c4.AddTag("Green");
             c4.AddTag("Combi");
             c4.AddTag("Family");
 
-            Image c5 = new Image("Purple Sport", prefix + "CarPurpleSport.jpg");
+            TaggedImage c5 = new TaggedImage("Purple Sport", prefix + "CarPurpleSport.jpg");
             c5.AddTag("Car");
             c5.AddTag("Purple");
             c5.AddTag("Sport");
             c5.AddTag("Family");
 
-            Image c6 = new Image("Yellow Sport", prefix + "CarYellowSport.jpg");
+            TaggedImage c6 = new TaggedImage("Yellow Sport", prefix + "CarYellowSport.jpg");
             c6.AddTag("Car");
             c6.AddTag("Yellow");
             c6.AddTag("Sport");
             c6.AddTag("Individual");
 
-            Image c7 = new Image("Black Sedan", prefix + "CarBlackSedan.jpg");
+            TaggedImage c7 = new TaggedImage("Black Sedan", prefix + "CarBlackSedan.jpg");
             c7.AddTag("Car");
             c7.AddTag("Black");
             c7.AddTag("Sedan");
             c7.AddTag("Family");
 
-            Image c8 = new Image("Light Blue Combi", prefix + "CarLightBlueCombi.jpg");
+            TaggedImage c8 = new TaggedImage("Light Blue Combi", prefix + "CarLightBlueCombi.jpg");
             c8.AddTag("Car");
             c8.AddTag("Light Blue");
             c8.AddTag("Combi");
             c8.AddTag("Family");
 
-            Image c9 = new Image("Magenta Pickup", prefix + "CarMagentaPickup.jpg");
+            TaggedImage c9 = new TaggedImage("Magenta Pickup", prefix + "CarMagentaPickup.jpg");
             c9.AddTag("Car");
             c9.AddTag("Magenta");
             c9.AddTag("Pickup");
@@ -109,23 +109,23 @@ namespace MVVMStarterDemoB.Configuration.App
             #endregion
 
             #region Employee image objects
-            Image s1 = new Image("Ann", prefix + "Ann.jpg");
+            TaggedImage s1 = new TaggedImage("Ann", prefix + "Ann.jpg");
             s1.AddTag("Employee");
             s1.AddTag("Female");
 
-            Image s2 = new Image("Benny", prefix + "Benny.jpg");
+            TaggedImage s2 = new TaggedImage("Benny", prefix + "Benny.jpg");
             s2.AddTag("Employee");
             s2.AddTag("Male");
 
-            Image s3 = new Image("Carol", prefix + "Carol.jpg");
+            TaggedImage s3 = new TaggedImage("Carol", prefix + "Carol.jpg");
             s3.AddTag("Employee");
             s3.AddTag("Female");
 
-            Image s4 = new Image("Dan", prefix + "Dan.jpg");
+            TaggedImage s4 = new TaggedImage("Dan", prefix + "Dan.jpg");
             s4.AddTag("Employee");
             s4.AddTag("Male");
 
-            Image s5 = new Image("Eliza", prefix + "Eliza.jpg");
+            TaggedImage s5 = new TaggedImage("Eliza", prefix + "Eliza.jpg");
             s5.AddTag("Employee");
             s5.AddTag("Female");
 
@@ -137,27 +137,27 @@ namespace MVVMStarterDemoB.Configuration.App
             #endregion
 
             #region Customer image objects
-            Image cu1 = new Image("Allan", prefix + "Allan.png");
+            TaggedImage cu1 = new TaggedImage("Allan", prefix + "Allan.png");
             cu1.AddTag("Customer");
             cu1.AddTag("Male");
 
-            Image cu2 = new Image("Betty", prefix + "Betty.png");
+            TaggedImage cu2 = new TaggedImage("Betty", prefix + "Betty.png");
             cu2.AddTag("Customer");
             cu2.AddTag("Female");
 
-            Image cu3 = new Image("Carl", prefix + "Carl.png");
+            TaggedImage cu3 = new TaggedImage("Carl", prefix + "Carl.png");
             cu3.AddTag("Customer");
             cu3.AddTag("Male");
 
-            Image cu4 = new Image("Denice", prefix + "Denice.png");
+            TaggedImage cu4 = new TaggedImage("Denice", prefix + "Denice.png");
             cu4.AddTag("Customer");
             cu4.AddTag("Female");
 
-            Image cu5 = new Image("Eric", prefix + "Eric.png");
+            TaggedImage cu5 = new TaggedImage("Eric", prefix + "Eric.png");
             cu5.AddTag("Customer");
             cu5.AddTag("Male");
 
-            Image cu6 = new Image("Fiona", prefix + "Fiona.png");
+            TaggedImage cu6 = new TaggedImage("Fiona", prefix + "Fiona.png");
             cu6.AddTag("Customer");
             cu6.AddTag("Female");
 
